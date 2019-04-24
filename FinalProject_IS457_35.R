d6078e12e5f2f57a6dfd818c3078b7fc6be0c92b
Phil Graff <pgraff@illinois.edu>
2019-04-23 22:23:55 -0500

    Q2 summaries done. 13:00

--- FinalProject_IS457_35.R
+++ FinalProject_IS457_35.R
@@ -133,6 +133,8 @@ for(i in 28:34){
 
 
 # 1.5: After dealing with missing values, show the dimensions of the data.
+
+# Show dimensions; I added a column for days listed
 dim(airbnb)
 
 # 1.6: Comment on and explain any other data cleaning or preparation steps you think would be 
@@ -147,16 +149,42 @@ dim(airbnb)
 
 # Q2
 # Conduct a preliminary exploration and describe what you find interesting or unexpected.
+
+# This will create two lists: counts (table()) for categorical data, and summaries for continuous data
+# This allows me to visually inspect the numerical distribution of each variable (using View())
 counts <- lapply(airbnb[,c(7,11,13:20,35)], function(x) table(x))
 summaries <- lapply(airbnb[,c(6,8,22:34,36)], function(x) summary(x))
 
-# Property types
-barplot(counts$property_type)
+counts
 
+# host_response_time: Most hosts respond quickly (within an hour), although the imputed unknown category makes up the second-largest percentage.
+# host_identity_verified: just under half of listing have a verified host. This even number could make for good comparisons if splitting the data.
+# zipcode: I don't plan to do much with zipcode. The distribution is uneven, and I don't know anything about the districts themselves.
+# property_type: Mostly Apartment (~58%) and House (~24%), although 31 property types total.
+# room_type: Three categories. Predominantly Entire home/apt, 26% Private room, and only 84 in a Shared room.
+# accommodates: Mostly 2, 4, 6, 3, 1, 5, then 8, 7, 10, and up to 16. Even numbers are more common.
+# bathrooms: Mostly 1 or 2, but goes up to 10!
+# bedrooms: Zero to four are "common" (>500), but continous up to 7 BR, then an outlier with 14 bedrooms.
+# beds: Zero to 14, then 16, 18, and 29(!). Interesting about zero beds; maybe a couch?
+# bed_type: Aha, couch is a type. 99.28% Real bed, with the remainder scattered around lesser bed types. There are 77 non-Real bed observations. It is plausible that the 69 zero beds observations are included in the 77.
+# cancellation_policy: Five categories -- flexible, moderate, strict and two levels of super_strict. I wonder if this affects reviews?
 
+summaries
 
-# Findings
-# With a 1st quartile of 100%, over 75% of listings have a 100% response rate
+# Summaries help with quartiles and ranges, skew can be somewhat interpreted by comparing median to mean.
+
+# host_since: median and mean almost identical; six years from min to median, 3.5 years median to max. Listings have been added more frequently over time.
+# host_response_rate: 1st quartile is 100%, so most hosts respond to inquiries.
+# price: median of 150 and mean of 203 indicates positive skew. This is expected on this type of variable; prices have no upper limit, but are bound on the lower end by zero.
+# cleaning_fee: same positive skew as price
+# guests_included: strong positive skew. median is 1, 3rd quar is 2, and mean is 1.969. Max of 16
+# extra_people: the max value of 410 may be influencing the mean
+# minimum_nights: skewed HEAVILY by the max of 500. A 500 night minimum seems to go against the typical use case for AirBnB that I understand. Higher minimum night values will suppress the possible number of reviews per month. I want to explore this.
+# number_of_reviews: median is 12; max is 493. Indicates popularity and longevity of a listing.
+# review_scores_rating: the overall rating on a scale of 20 to 100 (the minimum is 20). Can I assume this relates to a 5 star rating scale, where each star is worth 20 points?
+# review_scores_xxx: the xxx refers to subcategories of six review components: accuracy (of listing description), cleanliness, checkin, communication, location, and value.
+# reviews_per_month: effectively -- number_of_reviews / number of months listed. median .95, mean 1.5. max of 15.18 (averaging a new guest every other night!)
+
 
 # Q3
 # Explore comprehensively with charts, tables, and graphs
